48cf9f916e0a85711b8a1756433d77acb9242dc9
Ritesh Patil <85453534+RITESHPATIL117@users.noreply.github.com>
2025-02-06 14:32:40 +0000

    ew

--- data3.js
+++ data3.js
@@ -1,11 +1,11 @@
 let myYoutubename = "apgamer"
 let anothername = myYoutubename
-anothername = "chaiaurcode"
+anothername = "ultraaadi"
 
 console.log(myYoutubename);
 console.log(anothername);
 
 let userOne = {
-    email: "user@gmail.com"
+    email: "user@gmail.com",
     upi: "user@ybl"
 }
