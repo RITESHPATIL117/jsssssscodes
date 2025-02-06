f616be29547bba61189f77ff8694ad37ab757401
Ritesh Patil <85453534+RITESHPATIL117@users.noreply.github.com>
2025-02-06 14:14:39 +0000

    advacing slowely

---
+++ conversionop.js
@@ -0,0 +1,31 @@
+let score=undefined
+
+console.log(typeof score);
+console.log(typeof(score));
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+let isLoggedIn = ""
+
+let booleanIsLoggedIn= Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+
+let value = -3
+let negValue = value
+console.log(negValue)
+
+console.log(2%3)
+
+let str1 = "hello"
+let str2 = "Ritesh"
+let str3 = str1 +" "+ str2
+console.log(str3);
+
+console.log("1"+2);
+console.log(1+"2");
+console.log("1"+2+"2");
+console.log(1+2+"2");
+
+console.log((3+4)*5%3);
---
+++ data2.js
@@ -0,0 +1,24 @@
+const score = 100
+const scoreValue = 100.3
+
+const isLoggedIn = false
+const outsideTemp = null
+let userEmail;
+
+const id = Symbol('123')
+
+const anotherId= Symbol('123')
+console.log(id== anotherId);
+
+const heros=["shaktiman","baalveer","hero"];
+let myObj = {
+    name: "Ritesh",
+    age: 21,
+
+}
+
+const myFunction = function(){
+    console.log("Hello world");
+}
+
+ 
