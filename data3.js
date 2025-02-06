6e8c2213b66a34a92e8e4cdf3127101602ca6b1b
Ritesh Patil <85453534+RITESHPATIL117@users.noreply.github.com>
2025-02-06 14:32:26 +0000

    datatype

---
+++ data3.js
@@ -0,0 +1,11 @@
+let myYoutubename = "apgamer"
+let anothername = myYoutubename
+anothername = "chaiaurcode"
+
+console.log(myYoutubename);
+console.log(anothername);
+
+let userOne = {
+    email: "user@gmail.com"
+    upi: "user@ybl"
+}
