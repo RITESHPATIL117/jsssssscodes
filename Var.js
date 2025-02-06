c4038463b9dcd42ecbbd7ae426e4062b13b8cd98
Ritesh Patil <85453534+RITESHPATIL117@users.noreply.github.com>
2025-02-06 13:16:41 +0000

    prac day 2

---
+++ Var.js
@@ -0,0 +1,15 @@
+const accountId = 144553
+let accountEmail = "riteshpatiil112@gmail.com"
+var accountPassWord = "12334"
+ accountCity = "Jaipur"
+ let accounntState
+
+//accountId = 2
+accountEmail = "hvbufh@gmail.com"
+accountPassWord = "642345"
+accountCity = "Sangli"
+
+console.log(accountId);
+
+console.table([accountId,accountEmail,accountPassWord,accountCity,accounntState])
+
---
+++ data.js
@@ -0,0 +1,7 @@
+"use strict";
+
+console.log(3+3)
+
+let name ="Ritesh"
+let age = 18
+let isLoggedIn = false
